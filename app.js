020868dda30eb50d3bdf654090bf9c7f99c1e106
Schlukey <129208645+Schlukey@users.noreply.github.com>
2023-05-21 19:01:14 +0200

    quick bug fixes

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
-const port = 4200;
+const port = ( process.env.PORT );
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set(express.static("public"));
 
 //connect to the db
-const uri = `mongodb://localhost:27017/secretsDB`
+const uri = `mongodb://localhost:27017/secretsDB`;
 const client = new MongoClient(uri);
-const db = new client.db('SecretsLog');
-const collection = new client.collection('logins');
+const db = client.db("SecretsLog");
+const collection = db.collection("logins");
 app.get("/", async (req, res) => {
   res.render("home");
 });
@@ -32,13 +32,17 @@ app
       password: req.body.password,
     };
     //check the db to find the matching document
-    const response = await Collection.findOne(userLogin).then((user)=>{
-        console.log('match found')
-        res.redirect('secrets')
-    }).catch((err)=>{
+    const response = await collection
+      .findOne(userLogin)
+      .then((user) => {
+        console.log("match found");
+        res.redirect("secrets");
+      })
+      .catch((err) => {
         console.log(err);
-        res.redirect('login');
-    })
+        res.redirect("login");
+      });
+    return response;
   });
 
 app
@@ -53,13 +57,17 @@ app
       password: req.body.password,
     };
     //save that into a doc on the db
-    const response = await db.collection.save(loginParams).then(()=>{
-        console.log('new user added to db');
-        res.redirect('login');
-    }).catch((err)=>{
+    const response = await collection
+      .insertOne(loginParams)
+      .then(() => {
+        console.log("new user added to db");
+        res.redirect("login");
+      })
+      .catch((err) => {
         console.log(err);
-        res.redirect('register');
-    })
+        res.redirect("register");
+      });
+    return response;
   });
 
 app
